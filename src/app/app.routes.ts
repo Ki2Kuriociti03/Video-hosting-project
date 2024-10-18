64c0fee52080a6ba5d74e6de037e54976fab2b98
Kirill Borisiuk <kirilluborisyukal03@gmail.com>
2024-10-19 00:23:20 +0700

    create: created registration page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import {RouterModule, Routes} from '@angular/router';
 import { NgModule } from '@angular/core';
 import { AuthorizationComponent } from "./pages/authorization/authorization.component";
+import {RegistrationComponent} from "./pages/registration/registration.component";
 
 export const routes: Routes = [
   { path: 'authorization', component: AuthorizationComponent},
+  { path: 'reg', component: RegistrationComponent},
   { path: '', redirectTo: '/authorization', pathMatch: 'full' }
 ];
 
--- src/app/pages/registration/registration.component.ts
+++ src/app/pages/registration/registration.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-registration',
-  standalone: true,
-  imports: [],
   templateUrl: './registration.component.html',
-  styleUrl: './registration.component.scss'
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
+  styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent {
+  registrationForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.registrationForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, { validator: this.passwordMatchValidator });
+  }
+
+  // Проверка, что пароли совпадают
+  passwordMatchValidator(form: FormGroup) {
+    return form.get('password')?.value === form.get('confirmPassword')?.value
+      ? null : { mismatch: true };
+  }
+
+  // Обработка отправки формы
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      console.log('Form Submitted', this.registrationForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
